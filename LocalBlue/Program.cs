8447726fa6625d6c3595ddd0aef6ec21b62618a7
Dy-Stroyer12 <138273993+Dy-Stroyer12@users.noreply.github.com>
2024-10-12 23:03:43 -0400

    Proof of concept

--- LocalBlue/Program.cs
+++ LocalBlue/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using InTheHand.Net.Sockets;
+using System;
+
+Console.WriteLine("Searching for Bluetooth devices...");
+
+BluetoothClient bluetoothClient = new BluetoothClient();
+var devices = bluetoothClient.DiscoverDevices();
+
+if (devices.Count == 0)
+{
+    Console.WriteLine("No devices found.");
+}
+
+foreach (var device in devices)
+{
+    Console.WriteLine($"Found device: {device.DeviceName} ({device.DeviceAddress})");
+}
