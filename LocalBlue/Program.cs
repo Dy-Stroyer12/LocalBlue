7e2abca7ab34e15cedd8bf2ef30cd9733930fe03
Dy-Stroyer12 <138273993+Dy-Stroyer12@users.noreply.github.com>
2024-10-15 10:11:50 -0400

    Setting up more early structure

--- LocalBlue/Program.cs
+++ LocalBlue/Program.cs
@@ -1,17 +1,50 @@
-﻿using InTheHand.Net.Sockets;
+﻿using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
 using System;
+using System.Threading.Tasks;
+using System.Linq;
 
-Console.WriteLine("Searching for Bluetooth devices...");
+var appServiceGuid = new Guid("f6845322-085c-4847-907d-375132a872ab");
+
+BluetoothListener listener = new BluetoothListener(appServiceGuid);
+listener.ServiceName = "localblue";
+listener.Start();
+Console.WriteLine("Bluetooth listener started, waiting for incoming connections...");
+
+Console.WriteLine("Starting Bluetooth device discovery...");
 
 BluetoothClient bluetoothClient = new BluetoothClient();
 var devices = bluetoothClient.DiscoverDevices();
 
 if (devices.Count == 0)
 {
-    Console.WriteLine("No devices found.");
+    Console.WriteLine("No nearby Bluetooth devices found.");
 }
-
-foreach (var device in devices)
+else
 {
-    Console.WriteLine($"Found device: {device.DeviceName} ({device.DeviceAddress})");
+    Console.WriteLine("Discovered Bluetooth devices:");
+
+    foreach (var device in devices)
+    {
+        Console.WriteLine($"Found {device.DeviceName}. Checking services... ({device.DeviceAddress})");
+
+        try
+        {
+            var services = device.InstalledServices?.ToArray();
+
+            if (services != null && services.Contains(appServiceGuid))
+            {
+                Console.WriteLine($"- Device: '{device.DeviceName}' ({device.DeviceAddress}) - Running localblue");
+            }
+            else
+            {
+                Console.WriteLine($"- Device: {device.DeviceName} does not have the localblue service.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"- Unable to retrieve services for device: {device.DeviceName}. Error: {ex.Message}");
+        }
+    }
 }
