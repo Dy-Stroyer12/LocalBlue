c36c741487f72cd5508fc1f44d3411f5e2afa41b
Dy-Stroyer12 <138273993+Dy-Stroyer12@users.noreply.github.com>
2024-10-15 12:26:32 -0400

    Working on two way connections

---
+++ LocalBlue/RemoteClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using InTheHand.Net.Sockets;
+
+namespace LocalBlue
+{
+    public class RemoteClient
+    {
+        public string ClientName { get; set; }
+        public string DeviceAddress { get; set; }
+        public BluetoothClient Bridge { get; set; } = new BluetoothClient();
+
+        public RemoteClient()
+        {
+
+        }
+    }
+}
